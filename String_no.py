d89d30f84cf5254972805bb9898367c00d8f0440
Deepak Chauhan <88573326+Dazai5@users.noreply.github.com>
2021-08-11 18:25:06 +0530

    Update String_no.py
    Removed Comments

--- String_no.py
+++ String_no.py
@@ -1,16 +1,9 @@
 ip = input('Enter a string with numbers: ')
 search = input('Enter the number you want to search in the string: ')
 
-# for i in ip: --->My mistake
 for i in range(len(ip)):
     if search == ip[i]:
         print('The number is found at: ', ip[i])
         quit()
 
 print('Number not found')
-
-
-"""
-if search == ip[1]:
-    print('Hii')
-"""
